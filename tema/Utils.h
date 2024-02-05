3299d489ce352e2c866351e6dbaf57f4baf2fd26
Petraru Marcu-Darie <82521586+Marcu123@users.noreply.github.com>
2024-02-05 14:50:29 +0200

    Delete tema directory

--- tema/Utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include <iostream>
-using namespace std;
-
-void CheckErr(string ErrCode, int yylineno);
-string getUndefValue(string type);
--- tema/compile (4).sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "compiling $1"
-rm -f lex.yy.c
-rm -f $1.tab.c
-rm -f $1
-bison -d $1.y -Wcounterexamples -Wother
-lex $1.l
-g++ AST.cpp VarInfo.cpp VecInfo.cpp FuncInfo.cpp PackInfo.cpp SymbolTable.cpp Utils.cpp lex.yy.c  $1.tab.c  -o $1
